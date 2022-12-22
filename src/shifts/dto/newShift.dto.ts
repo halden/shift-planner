38ff57b41a04bc2354bb2b8bcac8f458162eb1f6
Aleksandr Tulpahh <tulpahh@gmail.com>
2022-12-22 12:32:01 +0200

    SP-0 Describe NewShiftDto fields

--- src/shifts/dto/newShift.dto.ts
+++ src/shifts/dto/newShift.dto.ts
@@ -1,19 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class NewShiftDto {
   @ApiProperty({
     description: 'Working date in Unix time',
     example: 1671400800,
   })
+  @IsNotEmpty()
+  @IsNumber()
   date: number;
 
   @ApiProperty({
+    description: 'Shift hours',
     type: Number,
+    example: 8,
   })
+  @IsNotEmpty()
+  @IsNumber()
   hours: number;
 
   @ApiProperty({
+    description: 'User ID for the shift',
     type: Number,
+    example: 1,
   })
+  @IsNotEmpty()
+  @IsNumber()
   userId: number;
 }
